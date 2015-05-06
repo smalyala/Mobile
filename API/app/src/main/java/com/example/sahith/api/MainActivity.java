58e66fb17afb56f6f2c9f2f2bb589e322a02467e
smalyala <sahith.malyala325@gmail.com>
2015-05-05 22:14:54 -0400

    twitter

--- API/app/src/main/java/com/example/sahith/api/MainActivity.java
+++ API/app/src/main/java/com/example/sahith/api/MainActivity.java
@@ -4,13 +4,23 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "UnQ0v7xpJ9gD4KUwxaI9LddEC";
+    private static final String TWITTER_SECRET = "2ZCi7Qa1uP9RxGS1kTQ28GlybZ1FLprkFgLHervaKUl53CorBe";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
     }
 
