1006072077e0d615efeb168b236b17d34d6bf663
smalyala <sahith.malyala325@gmail.com>
2015-05-05 20:16:53 -0400

    api

--- API/app/src/main/java/com/example/sahith/api/MainActivity.java
+++ API/app/src/main/java/com/example/sahith/api/MainActivity.java
@@ -1,36 +1,17 @@
 package com.example.sahith.api;
 
-
-import android.app.Activity;
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-
-import com.dropbox.sync.android.DbxAccount;
-import com.dropbox.sync.android.DbxAccountInfo;
-import com.dropbox.sync.android.DbxAccountManager;
-import com.dropbox.sync.android.DbxFile;
-import com.dropbox.sync.android.DbxFileInfo;
-import com.dropbox.sync.android.DbxFileSystem;
-import com.dropbox.sync.android.DbxPath;
-
-import java.io.IOException;
 
 
 public class MainActivity extends ActionBarActivity {
-    private DbxAccountManager myDbx;
-    static final int REQUEST_LINK_TO_DBX = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myDbx = DbxAccountManager.getInstance(getApplicationContext(), "hwpd7m3youi0qxg", "rqiyu92c92qrn3z");
     }
 
 
@@ -54,27 +35,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void onClickLinkToDropbox(View view)  {
-        myDbx.startLink((Activity) this, REQUEST_LINK_TO_DBX);
-        DbxAccount act = myDbx.getLinkedAccount();
-        DbxFileSystem files = DbxFileSystem.forAccount(act);
-        TextView text = (TextView)findViewById(R.id.textView);
-        DbxAccountInfo info = act.getAccountInfo();
-        text.setText(info.displayName);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_LINK_TO_DBX) {
-            if (resultCode == Activity.RESULT_OK) {
-                // ... Start using Dropbox files.
-            } else {
-                // ... Link failed or was cancelled by the user.
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
     }
 }
